e0527a1afb72a15685274fb84dfeac33b70ecf70
tholeteplok <teplok85@gmail.com>
2025-04-21 22:05:57 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Pastikan ini diimpor
 import App from './App';
 import './styles/main.css';
 import './styles/colors.css';
@@ -8,18 +9,29 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#ef5350',
+      main: '#ef5350', // Warna merah untuk Bridgestone
     },
     secondary: {
-      main: '#42a5f5',
+      main: '#42a5f5', // Warna biru untuk GT
     },
+    bridgestone: {
+      main: '#e31937', // Merah khas Bridgestone
+    },
+    dunlop: {
+      main: '#ffd700', // Kuning khas Dunlop
+    },
+    gt: {
+      main: '#005baa', // Biru khas Gajah Tunggal
+    }
   },
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter basename="/"> {/* Tambahkan BrowserRouter di sini */}
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
