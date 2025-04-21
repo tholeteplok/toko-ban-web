64ddad194bd811817240b3a216f117fa4893d66a
tholeteplok <teplok85@gmail.com>
2025-04-21 21:33:56 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/toko-ban-web/', // GANTI sesuai nama repo GitHub kamu
   plugins: [react()],
   server: {
-    port: 3000, // Port development
+    port: 3000, // port lokal
   },
   build: {
-    outDir: 'dist' // Folder output untuk production build
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src') // opsional, memudahkan import dengan "@"
+    }
   }
 })
